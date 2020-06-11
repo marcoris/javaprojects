7bc6366decd0d0ad745a590faa1fcdad94f03ca7
marcoris <info@risdesign.ch>
2020-06-11 16:08:32 +0200

    Add GUI1 project

---
+++ GUI1/src/gui1/MeineGUI.java
@@ -0,0 +1,12 @@
+package gui1;
+
+import javax.swing.JFrame;
+
+public class MeineGUI extends JFrame {
+    public static void main(String args[]) {
+        MeineGUI f = new MeineGUI();
+        f.setSize(300, 200);
+        f.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        f.setVisible(true);
+    }
+}
